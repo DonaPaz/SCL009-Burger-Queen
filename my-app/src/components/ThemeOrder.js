b59f3cf53bdffa61868a655830dcb137268c25a6
donapaz <danielapaz.pl@gmail.com>
2019-07-18 23:16:17 -0400

    Add first route attemp

---
+++ my-app/src/components/ThemeOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function ThemeOrder() {
+  return (
+    <div>
+      <h2>Desayuno o Cena</h2>
+    </div>
+  );
+}
+
+export default ThemeOrder;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 
 //Renderiza una aplicación y luego señala donde la debe mostrar
-ReactDOM.render(
-	<BrowserRouter>
-		<App />,
-		</BrowserRouter>,
-document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
